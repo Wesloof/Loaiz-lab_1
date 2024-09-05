40d3c670eaf972d8f9b891f55619967fd534ef0e
paxom <kladowkd@yandex.ru>
2024-09-05 18:56:51 +0300

    Задание 3 загружено в гит

--- lab1/Source.cpp
+++ lab1/Source.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main(void)
 {
-
-	int a[10] = {};
-	int min = 33000;
-	int max = 0;
+	setlocale(LC_ALL, "Russian");
+	size_t n;
+	cin >> n;
+	int* a = new int[n];
 	
-	for (int i = 0; i < 10; i++) { // 2 zadanie
+	for (int i = 0; i < n; i++) { // 2 zadanie
 		a[i] = rand();
-		cout << a[i] << "\n";
+		cout << "Элемент №" << i + 1 << " " << a[i] << "\n";
 	}
 
 	//cout << "minimum = " << min;
