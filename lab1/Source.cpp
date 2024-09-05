40699eeb596f63fd0599b6fb906fff26d171c825
paxom <kladowkd@yandex.ru>
2024-09-05 19:50:55 +0300

    Задание 5 загружено в гит, лаб работа сделана

--- lab1/Source.cpp
+++ lab1/Source.cpp
@@ -1,33 +1,73 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-int main(void)
+int main()
 {
 	setlocale(LC_ALL, "Russian");
-	int a[5][5];
-	int sum = 0;
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
+	int i;
+	string s1;
+	string s2;
+	string s3;
+	int n;
+	struct student
+	{
+		string famil;
+		string name, facult;
+		int Nomzach;
+	} stud[3];
 
-	for (int i = 0; i < 5; i++) { 
-		for (int j = 0; j < 5; j++){
-			a[i][j] = rand() % 10;
-			cout << a[i][j] << " ";
-		}
-		cout << endl;
+	for (i = 0; i < 3; i++)
+	{
+		cout << "Введите фамилию студента\n"; 
+		cin >> stud[i].famil;
 	}
-	cout << endl;
-	for (int i = 0; i < 5; i++) { // 2 sum strok
-		for (int j = 0; j < 5; j++) {
-			sum = sum + a[i][j];
-		}
-		cout << "Сумма строки " << i + 1 << " " << sum << endl;
-		sum = 0;
+	for (i = 0; i < 3; i++)
+	{
+		cout << "Введите имя студента " << stud[i].famil << endl;
+		cin >> stud[i].name;
 	}
-	for (int i = 0; i < 5; i++) { // 2 sum strok
-		for (int j = 0; j < 5; j++) {
-			sum = sum + a[j][i];
-		}
-		cout << "Сумма столбца " << i + 1 << " " << sum << endl;
-		sum = 0;
+	for (i = 0; i < 3; i++)
+	{
+		cout << "Введите название факультета студента " << stud[i].famil << " " << stud[i].name << endl;
+		cin >> stud[i].facult;
 	}
+	for (i = 0; i < 3; i++)
+	{
+		cout << "Введите номер зачётной книжки студента " << stud[i].famil << " " << stud[i].name << endl;
+		cin >> stud[i].Nomzach;
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		cout << "Cтудент " << stud[i].name << " обучается на факультете " << stud[i].facult << ", номер зачётной книжки " << stud[i].Nomzach << endl;
+	}
+
+	cout << "Введите имя, фамилию факультет и номер зачетной книги, которого хотите найти:" << endl;
+	cin >> s1;
+	cin >> s2;
+	cin >> s3;
+	cin >> n;
+	for (i = 0; i < 3; i++) {
+		if (strcmp(s1.c_str(), stud[i].famil.c_str()) == 0)
+		{
+			if (strcmp(s2.c_str(), stud[i].name.c_str()) == 0)
+			{
+				if (strcmp(s3.c_str(), stud[i].facult.c_str()) == 0)
+				{
+					if (n == stud[i].Nomzach)
+					{
+						cout << stud[i].famil << " "
+							<< stud[i].name << " "
+							<< stud[i].facult << " " 
+							<< stud[i].Nomzach << "Находится в базе";
+					}
+				}
+			}
+		}
+	}
+
 }
