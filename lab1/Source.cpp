321734f828ba70d531ccf61e21349d3a9d5878d7
paxom <kladowkd@yandex.ru>
2024-09-05 19:21:52 +0300

    Задание 4 загружено в гит

--- lab1/Source.cpp
+++ lab1/Source.cpp
@@ -5,15 +5,29 @@ using namespace std;
 int main(void)
 {
 	setlocale(LC_ALL, "Russian");
-	size_t n;
-	cin >> n;
-	int* a = new int[n];
-	
-	for (int i = 0; i < n; i++) { // 2 zadanie
-		a[i] = rand();
-		cout << "Элемент №" << i + 1 << " " << a[i] << "\n";
-	}
+	int a[5][5];
+	int sum = 0;
 
-	//cout << "minimum = " << min;
-	//cout << "\nmaximum = " << max;
+	for (int i = 0; i < 5; i++) { 
+		for (int j = 0; j < 5; j++){
+			a[i][j] = rand() % 10;
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < 5; i++) { // 2 sum strok
+		for (int j = 0; j < 5; j++) {
+			sum = sum + a[i][j];
+		}
+		cout << "Сумма строки " << i + 1 << " " << sum << endl;
+		sum = 0;
+	}
+	for (int i = 0; i < 5; i++) { // 2 sum strok
+		for (int j = 0; j < 5; j++) {
+			sum = sum + a[j][i];
+		}
+		cout << "Сумма столбца " << i + 1 << " " << sum << endl;
+		sum = 0;
+	}
 }
