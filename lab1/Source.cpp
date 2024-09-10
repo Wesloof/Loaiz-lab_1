a1a79092e74e903115980ff183314eee31fbca34
paxom <kladowkd@yandex.ru>
2024-09-10 12:00:50 +0300

    Задание к лабораторной выполнено(необходимо было перенести поиск в структуре в отдельную функцию)

--- lab1/Source.cpp
+++ lab1/Source.cpp
@@ -3,22 +3,50 @@
 
 using namespace std;
 
+struct student
+{
+	string famil;
+	string name, facult;
+	int Nomzach;
+}stud[3];
+
+void searchall(string name, string surn, string fac, int zac)
+{
+	for (int i = 0; i < 3; i++) {
+		if (zac == stud[i].Nomzach and
+			strcmp(name.c_str(), stud[i].name.c_str()) == 0 and
+			strcmp(surn.c_str(), stud[i].famil.c_str()) == 0 and
+			strcmp(fac.c_str(), stud[i].facult.c_str()) == 0)
+		{
+			cout << name << " "
+				<< surn << " "
+				<< "Находится в базе" << endl;
+		}
+	}
+}
+
+void searchonlyname(string name)
+{
+	for (int i = 0; i < 3; i++) {
+		if (strcmp(name.c_str(), stud[i].name.c_str()) == 0)
+		{
+			cout << name << " "
+				<< "Находится в базе";
+		}
+	}
+}
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	setvbuf(stdin, NULL, _IONBF, 0);
 	setvbuf(stdout, NULL, _IONBF, 0);
 	int i;
+	
 	string s1;
 	string s2;
 	string s3;
 	int n;
-	struct student
-	{
-		string famil;
-		string name, facult;
-		int Nomzach;
-	} stud[3];
 
 	for (i = 0; i < 3; i++)
 	{
@@ -51,23 +79,7 @@ int main()
 	cin >> s2;
 	cin >> s3;
 	cin >> n;
-	for (i = 0; i < 3; i++) {
-		if (strcmp(s1.c_str(), stud[i].famil.c_str()) == 0)
-		{
-			if (strcmp(s2.c_str(), stud[i].name.c_str()) == 0)
-			{
-				if (strcmp(s3.c_str(), stud[i].facult.c_str()) == 0)
-				{
-					if (n == stud[i].Nomzach)
-					{
-						cout << stud[i].famil << " "
-							<< stud[i].name << " "
-							<< stud[i].facult << " " 
-							<< stud[i].Nomzach << "Находится в базе";
-					}
-				}
-			}
-		}
-	}
+	searchall(s1, s2, s3, n);
+	searchonlyname(s1);
 
 }
